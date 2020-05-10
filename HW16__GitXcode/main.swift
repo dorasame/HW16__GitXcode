5ab7dd6814f5f5d660dd81a0255c04634aeb836c
Limdongju <dongju721@naver.com>
2020-05-10 04:18:06 -0700

    Initial Commit

---
+++ HW16__GitXcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HW16__GitXcode
+//
+//  Created by 20190708 on 10/05/2020.
+//  Copyright © 2020 DongjuLim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
